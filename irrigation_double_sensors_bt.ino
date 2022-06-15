711afba68afd8b0df34fb56ba67a496ed65aadec
gkirion <gkirion@gmail.com>
2022-06-16 01:24:15 +0300

    Send moisture of 2 places independently and include place name

--- irrigation_double_sensors_bt.ino
+++ irrigation_double_sensors_bt.ino
@@ -1,35 +1,42 @@
 #include <SoftwareSerial.h> 
 SoftwareSerial MyBlue(2, 3); // RX | TX 
 
+#define POT_WITH_TRAY A0
+#define POT_WITHOUT_TRAY A1
+#define POT_WITH_TRAY_POWER 6
+#define POT_WITHOUT_TRAY_POWER 7
+#define IRRIGATION_SIGNAL 8
 #define MAX_SENSOR_VALUE 1023
 
 char irrigation_status; // 1 ON | 0 OFF
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(6, OUTPUT);
-  pinMode(7, OUTPUT);
-  pinMode(8, OUTPUT);
-  digitalWrite(6, LOW);
-  digitalWrite(7, LOW);
-  digitalWrite(8, LOW);
+  pinMode(POT_WITH_TRAY_POWER, OUTPUT);
+  pinMode(POT_WITHOUT_TRAY_POWER, OUTPUT);
+  pinMode(IRRIGATION_SIGNAL, OUTPUT);
+  digitalWrite(POT_WITH_TRAY_POWER, LOW);
+  digitalWrite(POT_WITHOUT_TRAY_POWER, LOW);
+  digitalWrite(IRRIGATION_SIGNAL, LOW);
   irrigation_status = '0';
   MyBlue.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(6, HIGH);
-  digitalWrite(7, HIGH);
+  digitalWrite(POT_WITH_TRAY_POWER, HIGH);
+  digitalWrite(POT_WITHOUT_TRAY_POWER, HIGH);
   delay(10);
-  float sensor_1_input = analogRead(A0);
-  float sensor_2_input = analogRead(A1);
-  digitalWrite(6, LOW);
-  digitalWrite(7, LOW);
+  float pot_with_tray_moisture = analogRead(POT_WITH_TRAY);
+  float pot_without_tray_moisture = analogRead(POT_WITHOUT_TRAY);
+  digitalWrite(POT_WITH_TRAY_POWER, LOW);
+  digitalWrite(POT_WITHOUT_TRAY_POWER, LOW);
+  MyBlue.print("pot with tray,");
   MyBlue.print("moisture: ");
-  MyBlue.print(MAX_SENSOR_VALUE - sensor_1_input);
-  MyBlue.print(" ");
-  MyBlue.println(MAX_SENSOR_VALUE - sensor_2_input);
+  MyBlue.println(MAX_SENSOR_VALUE - pot_with_tray_moisture);
+  MyBlue.print("pot without tray,");
+  MyBlue.print("moisture: ");
+  MyBlue.println(MAX_SENSOR_VALUE - pot_without_tray_moisture);
   if (MyBlue.available() > 0) {
     char command = MyBlue.read();
     switch (command) {
@@ -39,12 +46,12 @@ void loop() {
         break;
         
        case '1':
-        digitalWrite(8, HIGH);
+        digitalWrite(IRRIGATION_SIGNAL, HIGH);
         irrigation_status = '1';
         break;
 
       case '0':
-        digitalWrite(8, LOW);
+        digitalWrite(IRRIGATION_SIGNAL, LOW);
         irrigation_status = '0';
         break; 
 
